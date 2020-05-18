ee416243d00e08bf0eb01741110b4fe3b08f46ee
Joao Poupino <joao@probely.com>
2020-05-18 16:19:07 +0000

    Add more debugging info

--- bin/start-tunnel.sh
+++ bin/start-tunnel.sh
@@ -14,17 +14,32 @@ if [ "${FARCASTER_REMOTE_GATEWAY}" = "" ]; then
     exit 1
 fi
 
+_debug=0
 if [ "$(echo ${FARCASTER_DEBUG} | grep -P '(1|on|enable|true)')" != "" ]; then
         set -x
+        echo "I am: $(id)"
+        _debug=1
 fi
 
 AGENT_HUB_HOST=$(echo ${FARCASTER_AGENT_HUB} | cut -d ':' -f 1)
 AGENT_HUB_PORT=$(echo ${FARCASTER_AGENT_HUB} | cut -d ':' -f 2)
 
-mkdir -p -m 0711 ${USER_SECRETS_PATH}
-cp -a ${SECRETS_PATH}/. ${USER_SECRETS_PATH}
-chmod -R go-rwx ${USER_SECRETS_PATH}
-chown -R remote-tunnel:remote-tunnel ${USER_SECRETS_PATH}
+if [ ! -d "${USER_SECRETS_PATH}" ]; then
+    mkdir -p -m 0711 -- "$(dirname ${USER_SECRETS_PATH})"
+    cp -a -- "${SECRETS_PATH}" "${USER_SECRETS_PATH}"
+    chmod -R go-rwx -- "${USER_SECRETS_PATH}"
+    chown -R remote-tunnel:remote-tunnel -- "${USER_SECRETS_PATH}"
+else
+    echo "${USER_SECRETS_PATH} already exists. Not setting up secrets..."
+fi
+
+if [ "${_debug}" = "1" ]; then
+    for d in / /run /run/farcaster /run/farcaster/secrets \
+        /farcaster /farcaster/restricted /farcaster/restricted/secrets; do
+        echo "${d}"
+        ls -la "${d}" || /bin/su -s /bin/sh remote-tunnel -c "ls -la ${d}" || echo "denied"
+    done
+fi
 
 exec /bin/su - -s /bin/sh remote-tunnel -c \
     "/usr/bin/ssh -N -F ${BASE_PATH}/etc/ssh/ssh_config \
